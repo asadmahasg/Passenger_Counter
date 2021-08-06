58de24d3ba26d54baee8b381012c02c0e242b45f
Asad Mahmood <asadasghar39@gmail.com>
2021-08-06 23:08:55 +0500

    new

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,2 +1,14 @@
 let count = 0
+let text = document.getElementById("value") 
+console.log(text)
 console.log(count)
+function Increment(){
+    count =  count + 1
+    console.log(count)
+    text.innerText = count
+}
+function Delete(){
+    count = 0
+    text.innerText = count
+
+}
